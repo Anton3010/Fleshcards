1322586232c667aaf1c7b073a17a929531470059
Anton Sharykin and Jamila Aliyeva <anton@>
2019-10-22 14:42:13 -0700

    will use js to add toggle to flashcards.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $(".clickable1").click(function() {
+    $(".js-expose").toggle();
+    $(".js-hidden").toggle();
+  });
+  $(".clickable2").click(function() {
+    $(".Operators-display").toggle();
+    $(".Operators-hidden").toggle();
+  });
+});
